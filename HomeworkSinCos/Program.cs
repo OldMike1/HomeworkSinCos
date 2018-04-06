0b3b94efc9d4ebdd0e168f2ec7fd50a2efa4cb1b
mike19952006 <mike19952006@gmail.com>
2018-04-06 19:43:07 +0300

    Задачка с технаря

--- HomeworkSinCos/Program.cs
+++ HomeworkSinCos/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace HomeworkSinCos
 {
+    class SinCos
+    {
+        public double GetResult(int n)
+        {
+            double res = 0;
+            int angle = 1;
+            double chislitel = Math.Cos(angle);
+            double znamenatel = Math.Sin(angle);
+            for (int i = 0; i < n; i++)
+            {
+                res += chislitel / znamenatel;
+                chislitel += Math.Cos(angle++);
+                znamenatel += Math.Sin(angle++);
+            }
+            return res;
+
+            //double res = 0;
+            
+            //double chislitel = 1;
+            //double znamenatel = 1;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    res += chislitel / znamenatel;
+            //    chislitel += 1;
+            //    znamenatel += 1;
+            //}
+            //return res;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            SinCos sincos = new SinCos();
+            double res = sincos.GetResult(n);
+            Console.WriteLine(res);
         }
     }
 }
